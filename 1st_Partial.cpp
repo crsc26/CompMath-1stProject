daf729ad7b88a66687c9f2aafb802257b9e3a586
Christian Ricardo Solís Cortés <Christian Ricardo Sols Corts>
2016-09-13 18:01:18 -0500

    Inicialización del programa

---
+++ 1st_Partial.cpp
@@ -0,0 +1,51 @@
+/*  First Partial Project
+    Christian Ricardo Solís Cortés A01063685
+    Samy Boucherit A01673900
+    Juan Carlos Estebes González A01204421
+    Javier Rodríguez A01152572
+    Marco Luna A01209537
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Grafo(string)
+{
+private:
+
+
+
+public:
+
+    Grafo(){} // Constructor
+
+    Grafo(string cadena)
+    {
+
+    }
+
+    string cadena; // Parámetros
+
+    void AFND()
+    {
+        // Función
+    }
+
+    ~Grafo() // Destructor
+    {
+        //cadena = 0;
+    }
+};
+
+int main()
+{
+    string cadena;
+    cout << "Indique la cadena:" << endl;
+    cin >> cadena << endl;
+
+    Grafo(cadena);
+
+    return 0;
+}
